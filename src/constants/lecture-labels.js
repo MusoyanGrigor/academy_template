0b727c33c8018320747e5df0b744c3448b3a116f
grigor <musoyangrigor251@gmail.com>
2025-12-16 15:35:29 +0400

    Add lecture-labels.js constant with lecture names

---
+++ src/constants/lecture-labels.js
@@ -0,0 +1,8 @@
+export const LectureLabels = Object.freeze({
+    LinearAlgebra: "Գծային հանրահաշիվ",
+    MathAnalysis: "Մաթեմատիկական անալիզ",
+    Probability: "Հավանականության տեսություն",
+    MachineLearning: "Մեքենայական ուսուցում",
+    DeepLearning: "Խորը ուսուցում",
+    Python: "Python"
+});
